bb96565cf964ede9177527ef82efec242fbe902b
hertonmota <metro.mota@gmail.com>
2022-05-16 19:41:54 -0300

    fix: username validation

--- schemas/login_schema.js
+++ schemas/login_schema.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 let login_schema = Yup.object().shape({
-    username: Yup.string().min(5).max(100).required('O Campo não foi inserido, digite e tente novamente.'),
+    username: Yup.string().min(5).max(100).matches(/^\d{9,11}|[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/).required('O Campo não foi inserido, digite e tente novamente.'),
     password: Yup.string().min(8).max(60).required('O Campo não foi inserido, digite e tente novamente.')
 })
 
